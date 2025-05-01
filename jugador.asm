180273645be8983eaa5f1b3a1593252b44b1b039
andresrojas03 <andresrojas030317@gmail.com>
2025-05-01 17:45:44 -0600

    Added bricks to the game, the ball now can collide and bounce with them changing their color

--- jugador.asm
+++ jugador.asm
@@ -64,6 +64,12 @@ player_ren      db      ini_renglon
 col_aux         db      0
 ren_aux         db      0
 
+brick_color 	db 		0
+mapa_bricks 	db 		3,2,1,3,2,1,'#',2,1,3,2,1,3,'#',1,3,2,1,3,2,'#',3,2,1,3,2,1,'#',2,1,3,2,1,3,'%' 
+;el número indica el "nivel" del brick, el carácter '#' indica el fin del renglón
+;el carácter '%' indica el fin del mapa
+
+
 ;Bola
 bola_col		db 		ini_columna 	 	;columna de la bola
 bola_ren		db 		ini_renglon-1 		;renglón de la bola
@@ -80,9 +86,7 @@ toco_lim_der	db 		0
 end_game 		db 		0
 delay_ticks		dw 		3
 aux_mul			dw		0
-
-
-
+dir_colision	db 		0	; 0 No colision, 1 colision superior, 2 colision inferior, 3 colision izquierda, 4 colision derecha
 ;////////macros//////
 ;limpiar pantalla
 clear macro
@@ -152,16 +156,13 @@ endm
 ;//////////////////////////////
 
 
-
-
-
-
     .code
 inicio: 
     inicializa_ds_es
 	clear
 	oculta_cursor_teclado	;oculta cursor del mouse
 	apaga_cursor_parpadeo 	;Deshabilita parpadeo del cursor
+	call IMPRIME_BRICKS
 	call BORRA_BOLA
 	call IMPRIME_BOLA
 	call IMPRIME_JUGADOR
@@ -238,7 +239,7 @@ PRINT_PLAYER proc
 	endp
 
 ;Borra la barra del jugador, que recibe como parámetros las variables ren_aux y col_aux, que indican la posición central de la barra
-	DELETE_PLAYER proc
+DELETE_PLAYER proc
 		posiciona_cursor [ren_aux],[col_aux]
 		imprime_caracter_color 20h,cBlanco,bgNegro
 		dec [col_aux]
@@ -276,7 +277,7 @@ BORRA_JUGADOR proc
 	endp
 
 ;Imprime la bola de juego, que recibe como parámetros las variables bola_col y bola_ren, que indican la posición de la bola
-	IMPRIME_BOLA proc
+IMPRIME_BOLA proc
 		mov ah,[bola_col]
 		mov al,[bola_ren]
 		mov [col_aux],ah
@@ -287,7 +288,7 @@ BORRA_JUGADOR proc
 	endp
 
 	;Borra la bola de juego, que recibe como parámetros las variables bola_col y bola_ren, que indican la posición de la bola
-	BORRA_BOLA proc
+BORRA_BOLA proc
 		mov ah,[bola_col]
 		mov al,[bola_ren]
 		mov [col_aux],ah
@@ -390,6 +391,10 @@ COMP_LIM_JUGADOR_DER proc
 			ret
 	endp
 
+
+
+
+
 ;//////////////////////////////////////////////
 ;////////////LOGICA DE LA BOLA/////////////////
 ;//////////////////////////////////////////////
@@ -399,142 +404,155 @@ COMP_LIM_JUGADOR_DER proc
 ;arriba se deben restar números y al revés
 
 MOVIMIENTO_BOLA proc
-    delay:
-        mov ah, 00h 
-        int 1Ah 
-        sub dx, [ticks]
-        cmp dx, [delay_ticks]
-        jb delay
+		delay:
+			mov ah, 00h 
+			int 1Ah 
+			sub dx, [ticks]
+			cmp dx, [delay_ticks]
+			jb delay
 
-        call BORRA_BOLA
-		
+			call BORRA_BOLA
+			
 
-        ; Movimiento según dirección actual
-        cmp [bola_dir], 0   ; Izquierda abajo
-        je direccion_0
-        cmp [bola_dir], 1   ; Derecha abajo
-        je direccion_1
-        cmp [bola_dir], 2   ; Izquierda arriba
-        je direccion_2
-        cmp [bola_dir], 3   ; Derecha arriba
-        je direccion_3
+			; Movimiento según dirección actual
+			cmp [bola_dir], 0   ; Izquierda abajo
+			je direccion_0
+			cmp [bola_dir], 1   ; Derecha abajo
+			je direccion_1
+			cmp [bola_dir], 2   ; Izquierda arriba
+			je direccion_2
+			cmp [bola_dir], 3   ; Derecha arriba
+			je direccion_3
 
-    direccion_0:
-        dec [bola_col]      ; Mover izquierda
-        inc [bola_ren]      ; Mover abajo
-        jmp comprobar_colision
+		direccion_0:
+			dec [bola_col]      ; Mover izquierda
+			inc [bola_ren]      ; Mover abajo
+			jmp comprobar_colision
 
-    direccion_1:
-        inc [bola_col]      ; Mover derecha
-        inc [bola_ren]      ; Mover abajo
-        jmp comprobar_colision
+		direccion_1:
+			inc [bola_col]      ; Mover derecha
+			inc [bola_ren]      ; Mover abajo
+			jmp comprobar_colision
 
-    direccion_2:
-        dec [bola_col]      ; Mover izquierda
-        dec [bola_ren]      ; Mover arriba
-        jmp comprobar_colision
+		direccion_2:
+			dec [bola_col]      ; Mover izquierda
+			dec [bola_ren]      ; Mover arriba
+			jmp comprobar_colision
 
-    direccion_3:
-        inc [bola_col]      ; Mover derecha
-        dec [bola_ren]      ; Mover arriba
-		jmp comprobar_colision
+		direccion_3:
+			inc [bola_col]      ; Mover derecha
+			dec [bola_ren]      ; Mover arriba
+			jmp comprobar_colision
 
-    comprobar_colision:
-		;Verificamos colision con la barra del jugador
-		mov al, [player_ren]
-        ; Verificar colisiones laterales primero
-        cmp [bola_col], lim_izquierdo
-        jle col_lat_izq
-        cmp [bola_col], lim_derecho
-        jge col_lat_der
+		comprobar_colision:
+			;Verificamos colision con la barra del jugador
+			mov al, [player_ren]
 
-        ; Luego verificar colisiones verticales
-        cmp [bola_ren], lim_superior
-        jle col_superior
+			;Verificar colisiones con los bricks
+			call DETECTA_COLISION_BRICKS
+			cmp [dir_colision], 1	;Si hubo colisión superior
+			je col_superior_escenario
+			cmp [dir_colision], 2	;Si hubo colision inferior
+			je col_inferior_escenario
+			cmp [dir_colision], 3	;Si hubo colision izquierda
+			je col_lat_izq_escenario
+			cmp [dir_colision], 4	;Si hubo colision derecha
+			je col_lat_der_escenario
 
-		;detectar si tocó al jugador para que rebote
-		call DETECTA_COLISION_JUGADOR
-        jc col_inferior			; si CF = 1 detectó al jugador
-		;detecta si se tocó el limite inferior para
-		;la logica de perder vida
-		cmp [bola_ren], lim_inferior
-		jge col_inferior
-        jmp dibujar_bola     ; Si no hay colisiones
+			; Verificar colisiones laterales del escenario
+			cmp [bola_col], lim_izquierdo
+			jle col_lat_izq_escenario
+			cmp [bola_col], lim_derecho
+			jge col_lat_der_escenario
 
-    col_lat_izq:
-        ; Cambiar dirección al rebotar en pared izquierda
-        cmp [bola_dir], 0    ; Si venía de izquierda-abajo
-        je cambiar_a_dir1    ; Cambiar a derecha-abajo
-        cmp [bola_dir], 2    ; Si venía de izquierda-arriba
-        je cambiar_a_dir3    ; Cambiar a derecha-arriba
-        jmp ajustar_posicion
+			; Luego verificar colisiones verticales
+			cmp [bola_ren], lim_superior
+			jle col_superior_escenario
 
-	col_lat_der:
-        ; Cambiar dirección al rebotar en pared derecha
-        cmp [bola_dir], 1    ; Si venía de derecha-abajo
-        je cambiar_a_dir0    ; Cambiar a izquierda-abajo
-        cmp [bola_dir], 3    ; Si venía de derecha-arriba
-        je cambiar_a_dir2    ; Cambiar a izquierda-arriba
-        jmp ajustar_posicion  
+			;detectar si tocó al jugador para que rebote
+			call DETECTA_COLISION_JUGADOR
+			jc col_inferior_escenario		; si CF = 1 detectó al jugador
 
-	col_superior:
-        ; Cambiar dirección al rebotar en techo
-        cmp [bola_dir], 2    ; Si venía de izquierda-arriba
-        je cambiar_a_dir0    ; Cambiar a izquierda-abajo
-        cmp [bola_dir], 3    ; Si venía de derecha-arriba
-        je cambiar_a_dir1    ; Cambiar a derecha-abajo
-        jmp ajustar_posicion
+			;detecta si se tocó el limite inferior para
+			;la logica de perder vida
+			cmp [bola_ren], lim_inferior
+			jge col_inferior_escenario
+			jmp dibujar_bola     ; Si no hay colisiones
 
-    col_inferior:
-        ; Cambiar dirección al rebotar en suelo
-        cmp [bola_dir], 0    ; Si venía de izquierda-abajo
-        je cambiar_a_dir2    ; Cambiar a izquierda-arriba
-        cmp [bola_dir], 1    ; Si venía de derecha-abajo
-        je cambiar_a_dir3    ; Cambiar a derecha-arriba
+		col_lat_izq_escenario:
+			; Cambiar dirección al rebotar en pared izquierda
+			cmp [bola_dir], 0    ; Si venía de izquierda-abajo
+			je cambiar_a_dir1    ; Cambiar a derecha-abajo
+			cmp [bola_dir], 2    ; Si venía de izquierda-arriba
+			je cambiar_a_dir3    ; Cambiar a derecha-arriba
+			jmp ajustar_posicion
 
-	cambiar_a_dir0:
-        mov [bola_dir], 0
-        jmp ajustar_posicion
+		col_lat_der_escenario:
+			; Cambiar dirección al rebotar en pared derecha
+			cmp [bola_dir], 1    ; Si venía de derecha-abajo
+			je cambiar_a_dir0    ; Cambiar a izquierda-abajo
+			cmp [bola_dir], 3    ; Si venía de derecha-arriba
+			je cambiar_a_dir2    ; Cambiar a izquierda-arriba
+			jmp ajustar_posicion  
 
-    cambiar_a_dir1:
-        mov [bola_dir], 1
-        jmp ajustar_posicion
+		col_superior_escenario:
+			; Cambiar dirección al rebotar en techo
+			cmp [bola_dir], 2    ; Si venía de izquierda-arriba
+			je cambiar_a_dir0    ; Cambiar a izquierda-abajo
+			cmp [bola_dir], 3    ; Si venía de derecha-arriba
+			je cambiar_a_dir1    ; Cambiar a derecha-abajo
+			jmp ajustar_posicion
 
-	cambiar_a_dir2:
-        mov [bola_dir], 2
-		jmp ajustar_posicion
+		col_inferior_escenario:
+			; Cambiar dirección al rebotar en suelo
+			cmp [bola_dir], 0    ; Si venía de izquierda-abajo
+			je cambiar_a_dir2    ; Cambiar a izquierda-arriba
+			cmp [bola_dir], 1    ; Si venía de derecha-abajo
+			je cambiar_a_dir3    ; Cambiar a derecha-arriba
 
-    cambiar_a_dir3:
-        mov [bola_dir], 3
-        jmp ajustar_posicion
+		cambiar_a_dir0:
+			mov [bola_dir], 0
+			jmp ajustar_posicion
 
-    ajustar_posicion:
-        ; Asegurar que la bola no se quede fuera de los límites
-        cmp [bola_col], lim_izquierdo
-        jg no_ajustar_izq
-        mov [bola_col], lim_izquierdo
-    no_ajustar_izq:
-        cmp [bola_col], lim_derecho
-        jl no_ajustar_der
-        mov [bola_col], lim_derecho
-    no_ajustar_der:
-        cmp [bola_ren], lim_superior
-        jg no_ajustar_sup
-        mov [bola_ren], lim_superior
-    no_ajustar_sup:
-        cmp [bola_ren], lim_inferior
-        jl no_ajustar_inf
-        mov [bola_ren], lim_inferior
-    no_ajustar_inf:
-        jmp dibujar_bola
+		cambiar_a_dir1:
+			mov [bola_dir], 1
+			jmp ajustar_posicion
 
-    dibujar_bola:
-        call IMPRIME_BOLA
-        mov ah, 00h 
-        int 1Ah 
-        mov [ticks], dx
-        ret
-endp
+		cambiar_a_dir2:
+			mov [bola_dir], 2
+			jmp ajustar_posicion
+
+		cambiar_a_dir3:
+			mov [bola_dir], 3
+			jmp ajustar_posicion
+
+		ajustar_posicion:
+			; Asegurar que la bola no se quede fuera de los límites
+			cmp [bola_col], lim_izquierdo
+			jg no_ajustar_izq
+			mov [bola_col], lim_izquierdo
+		no_ajustar_izq:
+			cmp [bola_col], lim_derecho
+			jl no_ajustar_der
+			mov [bola_col], lim_derecho
+		no_ajustar_der:
+			cmp [bola_ren], lim_superior
+			jg no_ajustar_sup
+			mov [bola_ren], lim_superior
+		no_ajustar_sup:
+			cmp [bola_ren], lim_inferior
+			jl no_ajustar_inf
+			mov [bola_ren], lim_inferior
+		no_ajustar_inf:
+			jmp dibujar_bola
+
+		dibujar_bola:
+			call IMPRIME_BOLA
+			mov ah, 00h 
+			int 1Ah 
+			mov [ticks], dx
+			ret
+	endp
 
 DETECTA_COLISION_JUGADOR proc
 		push ax
@@ -543,19 +561,11 @@ DETECTA_COLISION_JUGADOR proc
 		push dx 
 
 
-		; Guardar posición original de la bola
+		;Guardar posición original de la bola
 		mov dh, [bola_ren]
 		mov dl, [bola_col]
-		inc dh
+		inc dh						;incremento debajo de la bola para la colisión
 		
-		; Verificar posición debajo de la bola
-		;inc dh                 ; Mover temporalmente una fila abajo
-		
-		; Verificar los 3 puntos centrales del jugador (donde podría haber colisión)
-		;mov cx, 3                    ; Verificar 3 posiciones
-		;sub dl, 1                    ; Empezar una columna a la izquierda
-		
-	
 		;posicionar cursor
 		mov bh, 00h                  ; Página de video 0
 		mov ah, 02h                  ; Función para posicionar cursor
@@ -565,18 +575,18 @@ DETECTA_COLISION_JUGADOR proc
 		mov ah, 08h                  
 		int 10h
 		cmp ah, cBlanco              ; Comparar con color blanco
-		je colision_valida       	; Saltar si hay colisión
+		je colision_valida_jugador	 ;Colision con el jugador
 		
-		jmp no_colision
+		jmp no_colision_jugador
 		
-		colision_valida:
+		colision_valida_jugador:
 			cmp al, 223			;compara si tocó el caracter del jugador
-			jne no_colision     ;si no lo tocó no hay colisión   
+			jne no_colision_jugador    ;si no lo tocó no hay colisión   
 
 			stc					;CF = 1 porque hubo colision
 			jmp fin_deteccion
 
-		no_colision:
+		no_colision_jugador:
 			clc					;CF = 0 porque no hubo colisión
 		fin_deteccion:
 			pop dx
@@ -585,7 +595,306 @@ DETECTA_COLISION_JUGADOR proc
 			pop ax
 			ret
 
-endp
+	endp
 
 
+
+;////////////////////////////////////////////////
+;//////////////LOGICA BRICKS/////////////////////
+;////////////////////////////////////////////////
+IMPRIME_BRICKS proc
+		mov [col_aux],1
+		mov [ren_aux],2
+		mov di,0
+		mapa_sig_columna:
+			mov bl,[mapa_bricks+di]
+			cmp bl,3
+			je mapa_brick_n3
+			cmp bl,2
+			je mapa_brick_n2
+			cmp bl,1
+			je mapa_brick_n1
+			cmp bl,'#'
+			je mapa_fin_renglon
+			cmp bl,'%'
+			je mapa_fin
+		mapa_brick_n3:
+			mov [brick_color],cAzul
+			jmp mapa_imprime_brick
+		mapa_brick_n2:
+			mov [brick_color],cVerde
+			jmp mapa_imprime_brick
+		mapa_brick_n1:
+			mov [brick_color],cRojo
+		mapa_imprime_brick:
+			call PRINT_BRICK
+			add [col_aux],5
+			inc di
+			jmp mapa_sig_columna		
+		mapa_fin_renglon:
+			add [ren_aux],2
+			mov [col_aux],1
+			inc di
+			jmp mapa_sig_columna
+		mapa_fin:
+		ret
+	endp
+
+;Imprime un brick, que recibe como parámetros las variables ren_aux, col_aux y color_brick, que indican la posición superior izquierda del brick y su color
+PRINT_BRICK proc
+		mov ah,[col_aux]
+		mov al,[ren_aux]
+		push ax
+		posiciona_cursor [ren_aux],[col_aux]
+		imprime_caracter_color 219d,[brick_color],bgNegro
+		inc [col_aux]
+		posiciona_cursor [ren_aux],[col_aux]
+		imprime_caracter_color 219d,[brick_color],bgNegro
+		inc [col_aux]
+		posiciona_cursor [ren_aux],[col_aux]
+		imprime_caracter_color 219d,[brick_color],bgNegro
+		inc [col_aux]
+		posiciona_cursor [ren_aux],[col_aux]
+		imprime_caracter_color 219d,[brick_color],bgNegro
+		inc [col_aux]
+		posiciona_cursor [ren_aux],[col_aux]
+		imprime_caracter_color 219d,[brick_color],bgNegro
+		pop ax
+		mov [ren_aux],al
+		mov [col_aux],ah
+		ret
+	endp
+
+DETECTA_COLISION_BRICKS proc
+		push ax
+		push bx
+		push cx 
+		push dx 
+
+		; Guardar posición original y dirección
+		mov dh, [bola_ren]
+		mov dl, [bola_col]
+		mov ch, [bola_dir]
+
+		; Primera verificación (arriba de la bola)
+		dec dh                     ; Mover arriba para verificación
+		inc dl					
+		verificar_superior:
+			mov [dir_colision], 1
+			; Posicionar cursor
+			mov bh, 00h
+			mov ah, 02h
+			int 10h
+			
+			; Leer carácter y atributo
+			mov ah, 08h
+			int 10h
+			
+			call ES_UN_BRICK
+			jc determinar_direccion_colision
+			; Verificar colores de brick
+			;cmp ah, cAzul
+			;je verificar_caracter_brick
+			;cmp ah, cVerde
+			;je verificar_caracter_brick
+			;cmp ah, cRojo
+			;je verificar_caracter_brick
+			
+			; Si no hubo colisión superior, verificar inferior
+			jmp verificar_inferior
+			
+
+		verificar_caracter_brick:
+			cmp al, 219d            ; Verificar caracter del brick
+			jne verificar_inferior  ; Si no es el caracter, verificar inferior
+			jmp determinar_direccion_colision
+
+		verificar_inferior:
+			; Restaurar posición original y mover abajo
+			mov [dir_colision], 2
+			mov dh, [bola_ren]
+			mov dl, [bola_col]
+			add dh, 1               ; Mover abajo para verificación
+			
+			; Posicionar cursor
+			mov bh, 00h
+			mov ah, 02h
+			int 10h
+			
+			; Leer carácter y atributo
+			mov ah, 08h
+			int 10h
+			
+			call ES_UN_BRICK
+			jc determinar_direccion_colision
+			; Verificar colores de brick
+			;cmp ah, cAzul
+			;je verificar_caracter_brick_inf
+			;cmp ah, cVerde
+			;je verificar_caracter_brick_inf
+			;cmp ah, cRojo
+			;je verificar_caracter_brick_inf
+			
+			; Si no hubo colisión inferior pasamos a la verificación lateral
+			jmp verificar_izquierda
+
+		;verificar_caracter_brick_inf:
+		;	cmp al, 219d            ; Verificar caracter del brick
+		;	jne no_colision_bricks  ; Si no es el caracter, no hay colisión
+		;	jmp	determinar_direccion_colision 
+
+		verificar_derecha:
+			; Restaurar posición original y mover a la derecha
+			mov dh, [bola_ren]
+			mov dl, [bola_col]
+			mov [dir_colision], 4      ; Dirección 4 = derecha
+			inc dl                     ; Mover a la derecha
+			
+			; Posicionar cursor
+			mov bh, 00h
+			mov ah, 02h
+			int 10h
+			
+			; Leer carácter y atributo
+			mov ah, 08h
+			int 10h
+			
+			; Verificar si es brick (optimizado)
+			call es_un_brick
+			jc determinar_direccion_colision ; Si es brick, hay colisión
+			jmp no_colision_bricks         ; Si no, verificar izquierda
+
+		verificar_izquierda:
+			; Restaurar posición original y mover a la izquierda
+			mov dh, [bola_ren]
+			mov dl, [bola_col]
+			mov [dir_colision], 3      ; Dirección 3 = izquierda
+			dec dl                     ; Mover a la izquierda
+			
+			; Posicionar cursor
+			mov bh, 00h
+			mov ah, 02h
+			int 10h
+			
+			; Leer carácter y atributo
+			mov ah, 08h
+			int 10h
+			
+			; Verificar si es brick (usando misma función)
+			call es_un_brick
+			jc determinar_direccion_colision ; Si es brick, hay colisión
+			jmp verificar_derecha          ; Si no, pasar a verificar inferior
+
+
+		determinar_direccion_colision:
+			; Determinar tipo de colisión según dirección
+			cmp [dir_colision], 1
+			je colision_superior_brick
+			cmp [dir_colision], 2
+			je colision_inferior_brick
+			cmp [dir_colision], 3
+			je colision_izquierda_brick
+			cmp [dir_colision], 4
+			je colision_derecha_brick
+
+		colision_superior_brick:
+			call CAMBIAR_NIVEL_BRICK
+			stc                     ; Indicar colisión
+			jmp fin_deteccion_bricks
+
+		colision_inferior_brick:
+			call CAMBIAR_NIVEL_BRICK
+			stc                     ; Indicar colisión
+			jmp fin_deteccion_bricks
+
+		colision_izquierda_brick:		
+			call CAMBIAR_NIVEL_BRICK
+			stc                     ; Indicar colisión
+			jmp fin_deteccion_bricks
+		
+		colision_derecha_brick:	
+			call CAMBIAR_NIVEL_BRICK
+			stc                     ; Indicar colisión
+			jmp fin_deteccion_bricks
+
+		no_colision_bricks:
+			mov[dir_colision], 0 
+			clc                     ; Indicar no colisión
+
+		fin_deteccion_bricks:
+			pop dx
+			pop cx
+			pop bx
+			pop ax
+			ret
+	endp
+
+ES_UN_BRICK proc
+		es_un_brick:
+			; Verifica si el carácter en AH/AL es un brick válido
+			; Retorna CF=1 si es brick, CF=0 si no
+			cmp al, 219d            ; Primero verificar el carácter
+			jne no_es_brick
+			cmp ah, cAzul
+			je es_brick
+			cmp ah, cVerde
+			je es_brick
+			cmp ah, cRojo
+			je es_brick
+		
+		no_es_brick:
+			clc
+			ret
+		es_brick:
+			stc
+			ret
+
+	endp
+;Para disminuir el nivel del brick en caso de colisión
+CAMBIAR_NIVEL_BRICK proc
+		;Verificamos el nivel con el color guardado en el registro AH
+		mov [col_aux], dl 	;guardamos la columna donde fue la colisión
+		mov [ren_aux], dh	;guardamos el renglón donde fue la colisión
+		mov ch, ah 			;guardamos el color que se detectó
+		
+		; Calcular posición relativa dentro del brick (0-4)
+		mov al, [col_aux]
+		xor ah, ah          ; AX = col_aux
+		mov bl, 5
+		div bl              ; AH = col_aux % 5 (posición relativa)
+		
+		; Calcular inicio del brick (col_aux - posición relativa)
+		sub [col_aux], ah
+		inc [col_aux]
+		;Comparamos el color del brick para determinar el nivel de la colisión
+		cmp ch, cAzul		;colisión nivel 3 -> nivel 2
+		je nivel_2
+		
+		cmp ch, cVerde		;colisión nivel 2 -> nivel 1
+		je nivel_1
+		
+		cmp ch, cRojo		;colisión nivel 1 -> borrar Brick (pintar de negro)
+		je borra_brick
+
+		jmp fin_cambio
+
+		nivel_2:
+			mov [brick_color], cVerde
+			jmp imprimir_brick
+
+		nivel_1:
+			mov [brick_color], cRojo
+			jmp imprimir_brick
+
+		borra_brick:
+			mov [brick_color], cNegro
+			jmp imprimir_brick 
+
+		imprimir_brick:
+			call PRINT_BRICK		
+
+		fin_cambio:
+			ret 
+
+	endp
 end inicio; fin del programa
